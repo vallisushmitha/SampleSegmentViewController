c5efa1559f99448370f302db2c1683336d4a242d
kvanaMini1 <kvanamini1@kvanamini1s-mac-mini.local>
2016-05-23 16:08:14 +0530

    Initial Commit

---
+++ SampleSegmentViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleSegmentViewController
+//
+//  Created by kvanaMini1 on 23/05/16.
+//  Copyright © 2016 kvanaMini1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

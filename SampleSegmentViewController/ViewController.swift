6ef0dd2b513bcae917b749001338cf960d67aab9
kvanaMini1 <kvanamini1@kvanaMini1s-Mac-mini.local>
2016-07-18 17:15:55 +0530

    segmentedcontrol

--- SampleSegmentViewController/ViewController.swift
+++ SampleSegmentViewController/ViewController.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var mySegmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
+    var firstView: [String] = ["One","Two","Three","Four","Five"]
+    var secondView: [String] = ["Apple","Ball","Cat","Dog","Egg"]
+    var thirdView: [String] = ["A","B","C","D","E"]
+    var fourthView: [String] = ["a","b","c","d","e"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +31,61 @@ class ViewController: UIViewController {
     }
 
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       var returnvalue = 0
+        
+        switch (mySegmentControl.selectedSegmentIndex) {
+        case 0:
+            returnvalue = firstView.count
+            break
+        case 1:
+            returnvalue = secondView.count
+            break
+        case 2:
+            returnvalue = thirdView.count
+            break
+        case 3:
+            returnvalue = fourthView.count
+        default:
+            break
+        }
+        
+       return returnvalue
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+    let mycell = tableView.dequeueReusableCellWithIdentifier("mycell", forIndexPath: indexPath)
+        
+        
+        
+        switch (mySegmentControl.selectedSegmentIndex) {
+        case 0:
+            mycell.textLabel!.text = firstView[indexPath.row]
+            break
+        case 1:
+            mycell.textLabel!.text = secondView[indexPath.row]
+            break
+        case 2:
+            mycell.textLabel!.text = thirdView[indexPath.row]
+            break
+        case 3:
+            mycell.textLabel!.text = fourthView[indexPath.row]
+            break
+            
+        default:
+            break
+        }
+        return mycell
+    
+    }
+    
+    @IBAction func segmentedControl(sender: AnyObject) {
+        
+        myTableView.reloadData()
+        
+        
+    }
 }
 
